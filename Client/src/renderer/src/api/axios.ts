9cbe3b492931e2be34abb019cbf32409add86afa
Arviixzuh <arviixzuh@gmail.com>
2024-04-28 21:24:31 -0400

    feat: 🌱 implementación del objeto "headers" en la instancia de axios

--- Client/src/renderer/src/api/axios.ts
+++ Client/src/renderer/src/api/axios.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: import.meta.env.VITE_SERVER_API,
+  headers: {
+    Authorization: localStorage.getItem('token'),
+  },
 })
