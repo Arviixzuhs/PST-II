0534d85140f403b5d594237d243b40ddc8656680
Arviixzuh <arviixzuh@gmail.com>
2024-05-12 03:38:42 -0400

    feat: 📖 implementación de reducers para manejar las consultas médicas

--- Client/src/renderer/src/store/index.ts
+++ Client/src/renderer/src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { manageUserSlice } from '@renderer/features/userSlice'
 import { manageUsersSlice } from '@renderer/features/usersSlice'
+import { manageConsultsSlice } from '@renderer/features/consultSlice'
 
 const store = configureStore({
   reducer: {
     user: manageUserSlice.reducer,
     users: manageUsersSlice.reducer,
+    consult: manageConsultsSlice.reducer,
   },
 })
 
