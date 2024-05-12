f5f49ff342d84c643a8462741b1b1f0a51e6239e
Arviixzuh <arviixzuh@gmail.com>
2024-05-12 03:37:11 -0400

    feat: ✨ implementación de requests para manejar las consultas médicas

--- Client/src/renderer/src/api/Requests.ts
+++ Client/src/renderer/src/api/Requests.ts
@@ -11,3 +11,11 @@ export const reqLoadAllPatients = () => api.get('/patient/get-all')
 export const authLogin = async (data: any) => api.post('/auth/login', data)
 export const authRegister = async (data: any) => api.post('/auth/register', data)
 export const authLoadProfileByToken = async (data: any) => api.get('/auth/load/profile/' + data)
+
+/* Rutas para manejar las consultas */
+export const reqCreateConsult = async (data: any) => api.post('/consult/create', data)
+export const reqDeleteConsult = async (id: any) => api.delete('/consult/delete/' + id)
+export const reqUpdateConsult = async ({ data, id }: { data: any; id: any }) =>
+  api.put('/consult/update/' + id, data)
+export const reqGetAllConsults = async () => api.get('/consult/get-all')
+export const reqGetConsult = async (id) => api.get('/consult/get/' + id)
