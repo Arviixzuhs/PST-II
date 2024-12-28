10d0e0fec73623493da7bb535296ec4f54ad0860
Arviixzuh <arviixzuh@gmail.com>
2024-12-28 19:42:26 -0400

    refactor: 🛠️ remove unused API call and update calendar default value handling

--- frontend/src/renderer/src/components/Calendar/index.tsx
+++ frontend/src/renderer/src/components/Calendar/index.tsx
@@ -1,10 +1,7 @@
-import React from 'react'
 import { Calendar } from '@nextui-org/react'
 import { parseDate } from '@internationalized/date'
 import { RootState } from '@renderer/store'
-import { setConsults } from '@renderer/features/consultSlice'
 import { newParseDate } from '@renderer/utils/newParseDate'
-import { reqGetAllConsults } from '@renderer/api/Requests'
 import { setCurrentConsultDate } from '@renderer/features/consultSlice'
 import { useDispatch, useSelector } from 'react-redux'
 
@@ -12,14 +9,6 @@ export const MedicalConsultationCalendar = () => {
   const dispatch = useDispatch()
   const consults = useSelector((state: RootState) => state.consult)
   const defaultDate = `${consults.currentConsultDate?.year}-${consults.currentConsultDate?.month?.toString().padStart(2, '0')}-${consults.currentConsultDate?.day?.toString().padStart(2, '0')}`
-
-  React.useEffect(() => {
-    const loadAllConsults = async () => {
-      const response = await reqGetAllConsults()
-      dispatch(setConsults(response.data))
-    }
-    loadAllConsults()
-  }, [])
 
   const isDateUnavailable = (date: any) => {
     return !consults.data.some((consult: any) => {
@@ -38,7 +27,7 @@ export const MedicalConsultationCalendar = () => {
       onChange={(e) =>
         dispatch(setCurrentConsultDate({ day: e.day, month: e.month, year: e.year }))
       }
-      defaultValue={consults.currentConsultDate && parseDate(defaultDate)}
+      value={consults.currentConsultDate && parseDate(defaultDate)}
       isDateUnavailable={isDateUnavailable}
     />
   )
