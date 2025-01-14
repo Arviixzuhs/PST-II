692f8c59910d1977818b7b0b96e73c65f3ccb56b
Arviixzuh <arviixzuh@gmail.com>
2025-01-14 18:16:56 -0400

    chore: 🌱 update TableProps interfaces for improved type safety and clarity in modal input handling

--- frontend/src/renderer/src/components/TableUser/interfaces/TableProps.ts
+++ frontend/src/renderer/src/components/TableUser/interfaces/TableProps.ts
@@ -6,27 +6,37 @@ export interface ColumnData {
 
 export interface ColumnsData {
   columns: ColumnData[]
-  InitialVisibleColumns: string[]
   statusOptions?: { name: string; uid: string }[]
+  InitialVisibleColumns: string[]
 }
 
 export interface TableActions {
+  edit: (data: unknown, currentUserEdit: unknown) => void
   delete: (id: number) => void
   create: (data: unknown) => void
-  edit: (data: unknown, currentUserEdit: unknown) => void
+}
+
+export interface ModalInputProps {
+  name: string;
+  type: string;
+  label: string,
+  isRequired?: boolean
+  placeholder?: string;
+}
+
+export interface ModalSelectProps {
+  name: string
+  label: string
+  options: { value: string; label: string }[]
+  placeholder?: string
 }
 
 export interface ModalProps {
   title: string
   action: (...data: unknown[]) => void
-  inputs?: { name: string; placeholder?: string; type: string; label: string }[]
+  inputs?: ModalInputProps[]
   buttonTitle?: string
-  selectInputs?: {
-    name: string
-    placeholder?: string
-    label: string
-    options: { value: string; label: string }[]
-  }[]
+  selectInputs?: ModalSelectProps[]
 }
 
 export interface AppTableInterface {
