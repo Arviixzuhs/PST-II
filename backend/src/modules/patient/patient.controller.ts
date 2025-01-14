0893ffd1a1811406b4630f4815366caa24b90c97
Arviixzuh <arviixzuh@gmail.com>
2025-01-14 14:47:49 -0400

    refactor: 🛠️ enhance type safety in PatientController by updating method signatures to return promises

--- backend/src/modules/patient/patient.controller.ts
+++ backend/src/modules/patient/patient.controller.ts
@@ -1,3 +1,4 @@
+import { Patient } from '@prisma/client'
 import { PatientDto } from './dto/patient.dto'
 import { PatientService } from './patient.service'
 import { EditPatientDto } from './dto/edit-patient.dto'
@@ -9,45 +10,45 @@ import { Get, Put, Post, Body, Param, Query, Delete, Controller } from '@nestjs/
 @ApiTags('Patient')
 @ApiBearerAuth()
 export class PatientController {
-  constructor(private readonly patientService: PatientService) {}
+  constructor(private readonly patientService: PatientService) { }
 
   @Post('/register')
-  register(@Body() data: PatientDto) {
+  register(@Body() data: PatientDto): Promise<Patient> {
     return this.patientService.patientRegister(data)
   }
 
   @Get('/get/:id')
-  getUserById(@Param('id') id: number) {
+  getUserById(@Param('id') id: number): Promise<Patient> {
     return this.patientService.getPatientById(id)
   }
 
   @Get('/get-all')
-  getAllUsers() {
+  getAllUsers(): Promise<Patient[]> {
     return this.patientService.getAllPatients()
   }
 
   @Delete('/delete/:id')
-  deleteUser(@Param('id') id: number) {
+  deleteUser(@Param('id') id: number): Promise<Patient> {
     return this.patientService.deletePatient(id)
   }
 
   @Get('/count-by-date')
-  getPatientsCountByDate() {
+  getPatientsCountByDate(): Promise<{ time: string; value: number }[]> {
     return this.patientService.getPatientsCountByDate()
   }
 
   @Get('/count-by-gender')
-  getPatientsCountByGender() {
+  getPatientsCountByGender(): Promise<{ gender: string; count: number }[]> {
     return this.patientService.getPatientsCountByGender()
   }
 
   @Get('/search-by-name')
-  searchPatientByName(@Query() query: SearchPatientDto) {
+  searchPatientByName(@Query() query: SearchPatientDto): Promise<Patient[]> {
     return this.patientService.searchPatientByName(query.name)
   }
 
   @Put('/update/:id')
-  updateUser(@Param('id') id: number, @Body() data: EditPatientDto) {
+  updateUser(@Param('id') id: number, @Body() data: EditPatientDto): Promise<Patient> {
     return this.patientService.updatePatient(id, data)
   }
 }
