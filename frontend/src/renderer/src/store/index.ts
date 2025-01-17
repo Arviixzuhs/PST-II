63d114d5a434828744b074f7289b3083ac99ce6f
Arviixzuh <arviixzuh@gmail.com>
2025-01-17 01:19:45 -0400

    feat: 🏷️ integrate manageItems and manageModals slices into the Redux store

--- frontend/src/renderer/src/store/index.ts
+++ frontend/src/renderer/src/store/index.ts
@@ -1,5 +1,7 @@
 import { manageUserSlice } from '@renderer/features/userSlice'
+import { manageItemsSlice } from '@renderer/features/tablePerTabSlice'
 import { manageUsersSlice } from '@renderer/features/usersSlice'
+import { manageModalsSlice } from '@renderer/features/currentModal'
 import { manageConsultsSlice } from '@renderer/features/consultSlice'
 import { manageHospitalDataSlice } from '@renderer/features/hospitalSlice'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
@@ -7,8 +9,10 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit'
 const appReducer = combineReducers({
   user: manageUserSlice.reducer,
   users: manageUsersSlice.reducer,
+  modals: manageModalsSlice.reducer,
   consult: manageConsultsSlice.reducer,
   hospital: manageHospitalDataSlice.reducer,
+  tablePerTab: manageItemsSlice.reducer,
 })
 
 const store = configureStore({
