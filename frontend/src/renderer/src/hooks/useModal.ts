2c7d8f092842ed6f546ea45d6f6fad9ed1395bc0
Arviixzuh <arviixzuh@gmail.com>
2025-01-17 01:18:04 -0400

    feat: ✨ add modal management slice and custom hook for modal state handling

---
+++ frontend/src/renderer/src/hooks/useModal.ts
@@ -0,0 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { handleShowModal, isModalOpen } from '@renderer/features/currentModal'
+
+export const modalTypes = Object.freeze({
+  editItemTableModal: 'EDIT_ITEM_TABLE_MODAL',
+  editItemTableTabModal: 'EDIT_ITEM_TABLE__TAB_MODAL',
+  viewPatientProfileModal: 'VIEW_PATIENT_PROFILE_MODAL',
+} as const)
+
+type ModalType = (typeof modalTypes)[keyof typeof modalTypes]
+
+export const useModal = (modalType: ModalType): [boolean, () => void] => {
+  const dispatch = useDispatch()
+  const isOpen = useSelector(isModalOpen(modalType))
+
+  const toggleModal = () => dispatch(handleShowModal(modalType))
+
+  return [isOpen, toggleModal]
+}
