8d4e6406fafd4cd26cf7297315d6d67a34924b2e
Arviixzuh <arviixzuh@gmail.com>
2025-01-17 01:16:45 -0400

    feat: 📜 add DropdownItemInterface for ActionDropdown component

---
+++ frontend/src/renderer/src/components/TableUser/interfaces/ActionDropdown.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react'
+
+export interface DropdownItemInteface {
+  key: string
+  title: string
+  onPress: (args: number) => Promise<void>
+  startContent?: ReactNode | undefined
+}
