b2d59faf4964b9bad74268d1506066d4a9b9ef09
Arviixzuh <arviixzuh@gmail.com>
2025-01-17 01:17:15 -0400

    feat: 🌈 extend TableProps interface to include dropdown actions and enhance modal input types

--- frontend/src/renderer/src/components/TableUser/interfaces/TableProps.ts
+++ frontend/src/renderer/src/components/TableUser/interfaces/TableProps.ts
@@ -1,3 +1,5 @@
+import { DropdownItemInteface } from './ActionDropdown'
+
 export interface ColumnData {
   uid: string
   name: string
@@ -14,14 +16,46 @@ export interface TableActions {
   edit: (data: unknown, currentUserEdit: unknown) => void
   delete: (id: number) => void
   create: (data: unknown) => void
+  search?: (value: string) => void
 }
 
-export interface ModalInputProps {
-  name: string;
-  type: string;
-  label: string,
+export interface ModalProps {
+  title: string
+  action: (...data: unknown[]) => void
+  inputs?: FormField[]
+  textArea?: TextAreaField[]
+  buttonTitle?: string
+  selectInputs?: SelectField[]
+}
+
+interface BaseField {
+  name: string
+  label?: string
   isRequired?: boolean
-  placeholder?: string;
+  placeholder?: string
+}
+
+interface FormField extends BaseField {
+  type: string
+}
+
+interface SelectField extends BaseField {
+  options: SelectOption[]
+}
+
+interface SelectOption {
+  label: string
+  value: string
+}
+
+export type TextAreaField = BaseField
+
+export interface ModalInputProps {
+  name: string
+  type: string
+  label: string
+  isRequired?: boolean
+  placeholder?: string
 }
 
 export interface ModalSelectProps {
@@ -31,17 +65,10 @@ export interface ModalSelectProps {
   placeholder?: string
 }
 
-export interface ModalProps {
-  title: string
-  action: (...data: unknown[]) => void
-  inputs?: ModalInputProps[]
-  buttonTitle?: string
-  selectInputs?: ModalSelectProps[]
-}
-
 export interface AppTableInterface {
   columnsData: ColumnsData
   tableActions: TableActions
   addItemModal: React.JSX.Element
   editItemModal: React.JSX.Element
+  dropdownAction?: DropdownItemInteface[] | undefined
 }
