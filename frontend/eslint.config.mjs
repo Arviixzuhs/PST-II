97b5c86f6e353cbec4d8ac2f24f50a2620de0ed8
Arviixzuh <arviixzuh@gmail.com>
2025-01-17 01:07:42 -0400

    chore: 🎨 add ESLint rule to enforce no unused variables with specific ignore patterns

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -15,6 +15,18 @@ export default [
       'react/prop-types': 'off',
       'react/react-in-jsx-scope': 'off',
       'react/jsx-uses-react': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
     },
   },
 ]
