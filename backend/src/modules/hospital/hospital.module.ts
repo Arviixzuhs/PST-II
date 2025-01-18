b3e284dd284f3cfbf9bfd8488212c54fb400725c
Arviixzuh <arviixzuh@gmail.com>
2025-01-18 00:47:09 -0400

    feat: 🥞 add PatientService and ClinicalStaffService to HospitalModule providers

--- backend/src/modules/hospital/hospital.module.ts
+++ backend/src/modules/hospital/hospital.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common'
 import { PrismaModule } from 'src/prisma/prisma.module'
 import { HospitalService } from './hospital.service'
 import { HospitalController } from './hospital.controller'
+import { PatientService } from '../patient/patient.service'
+import { ClinicalStaffService } from '../clinicalStaff/clinicalStaff.service'
 
 @Module({
   controllers: [HospitalController],
-  providers: [HospitalService],
+  providers: [HospitalService, PatientService, ClinicalStaffService],
   imports: [PrismaModule],
 })
 export class HospitalModule {}
