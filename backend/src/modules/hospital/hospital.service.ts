e69d11c8b95a0cef33fcb33a1222961c70b4ec21
Arviixzuh <arviixzuh@gmail.com>
2025-01-18 00:46:48 -0400

    feat: ✨ enhance HospitalService to include detailed patient and clinical staff statistics

--- backend/src/modules/hospital/hospital.service.ts
+++ backend/src/modules/hospital/hospital.service.ts
@@ -1,21 +1,51 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { ClinicalStaffService } from '../clinicalStaff/clinicalStaff.service'
+import { PatientService } from '../patient/patient.service'
 
 @Injectable()
 export class HospitalService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly clinicalStaffService: ClinicalStaffService,
+    private readonly patientService: PatientService,
+  ) {}
 
   async getInfo(): Promise<{
     rooms: number
     consults: number
     patients: number
     clinicalStaffs: number
+    patientsCount: { time: string; value: number }[]
+    clinicalStaffsCount: { time: string; value: number }[]
+    patientsCountByGender: { gender: string; count: number }[]
   }> {
-    const rooms = (await this.prisma.room.findMany()).length
-    const consults = (await this.prisma.consult.findMany()).length
-    const patients = (await this.prisma.patient.findMany()).length
-    const clinicalStaffs = (await this.prisma.clinicalStaff.findMany()).length
+    const [
+      { rooms, consults, patients, clinicalStaffs },
+      patientsCount,
+      clinicalStaffsCount,
+      patientsCountByGender,
+    ] = await Promise.all([
+      this.prisma.$transaction(async (prisma) => {
+        const rooms = await prisma.room.count()
+        const consults = await prisma.consult.count()
+        const patients = await prisma.patient.count()
+        const clinicalStaffs = await prisma.clinicalStaff.count()
+        return { rooms, consults, patients, clinicalStaffs }
+      }),
+      this.patientService.getPatientsCountByDate(),
+      this.clinicalStaffService.getClinicalStaffCountByDate(),
+      this.patientService.getPatientsCountByGender(),
+    ])
 
-    return { rooms, consults, patients, clinicalStaffs }
+    return {
+      rooms,
+      consults,
+      patients,
+      clinicalStaffs,
+      patientsCount,
+      clinicalStaffsCount,
+      patientsCountByGender,
+    }
   }
 }
