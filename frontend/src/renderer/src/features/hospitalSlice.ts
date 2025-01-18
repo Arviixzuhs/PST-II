4148d86d858667554c5a0bdca301f75a0cf2c491
Arviixzuh <arviixzuh@gmail.com>
2025-01-18 00:46:42 -0400

    feat: ✨ add hospital statistics interfaces and update initial state in hospitalSlice

--- frontend/src/renderer/src/features/hospitalSlice.ts
+++ frontend/src/renderer/src/features/hospitalSlice.ts
@@ -1,13 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+export interface CountItem {
+  time: string
+  value: number
+}
+
+export interface PatientsCountByGender {
+  gender: string
+  count: number
+}
+
+export interface HospitalStats {
+  rooms: number
+  patients: number
+  consults: number
+  clinicalStaffs: number
+  patientsCount: CountItem[]
+  clinicalStaffsCount: CountItem[]
+  patientsCountByGender: PatientsCountByGender[]
+}
+
 export const manageHospitalDataSlice = createSlice({
   name: 'user',
   initialState: {
     rooms: 0,
     patients: 0,
     consults: 0,
+    patientsCount: [],
     clinicalStaffs: 0,
-  },
+    clinicalStaffsCount: [],
+    patientsCountByGender: [],
+  } as HospitalStats,
   reducers: {
     setHospitalStats: (_, action) => action.payload,
   },
