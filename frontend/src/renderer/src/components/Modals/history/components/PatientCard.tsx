fbb91c3a9407bb674768d67f95ad1e63ae39043b
Arviixzuh <arviixzuh@gmail.com>
2025-01-19 18:42:15 -0400

    feat: 🥞 add patient status, reason for entry, and cause of death attributes

--- frontend/src/renderer/src/components/Modals/history/components/PatientCard.tsx
+++ frontend/src/renderer/src/components/Modals/history/components/PatientCard.tsx
@@ -1,14 +1,18 @@
-import { Avatar } from '@nextui-org/react'
+import { Patient } from '@renderer/interfaces/patientModel'
 import { RootState } from '@renderer/store'
 import { useSelector } from 'react-redux'
-import { Card, CardBody, Chip } from '@nextui-org/react'
+import { newParseDateWithTime } from '@renderer/utils/newParseDate'
+import { Avatar, Card, CardBody, Chip, Tooltip } from '@nextui-org/react'
 
 export const PatientCard = () => {
   const users = useSelector((state: RootState) => state.users.data)
   const currentUserIdEdit = useSelector((state: RootState) => state.users.currentUserIdEdit)
-  const currentUserEdit = users.find((item) => item.id == currentUserIdEdit)
+  const currentUserEdit = users.find((item) => item.id == currentUserIdEdit) as Patient
 
-  const statusColorMap: { [key: string]: { text: string; color: 'secondary' | 'primary' } } = {
+  const parsedDate = newParseDateWithTime({ date: currentUserEdit?.createdAt })
+  const statusColorMap: {
+    [key: string]: { text: string; color: 'secondary' | 'primary' | 'success' | 'danger' }
+  } = {
     FEMALE: {
       text: 'Femenino',
       color: 'secondary',
@@ -16,6 +20,14 @@ export const PatientCard = () => {
     MALE: {
       text: 'Masculino',
       color: 'primary',
+    },
+    ALIVE: {
+      text: 'Vivo',
+      color: 'success',
+    },
+    DEAD: {
+      text: 'Fallecido',
+      color: 'danger',
     },
   }
 
@@ -37,6 +49,35 @@ export const PatientCard = () => {
                   {statusColorMap[currentUserEdit.gender].text}
                 </Chip>
               )}
+              {currentUserEdit?.status && (
+                <>
+                  <Tooltip
+                    className='default-text-color max-w-xs'
+                    content={currentUserEdit.reasonDeath}
+                    isDisabled={
+                      currentUserEdit.status === 'ALIVE' || currentUserEdit.reasonDeath.length === 0
+                    }
+                  >
+                    <Chip
+                      size='sm'
+                      variant='flat'
+                      color={statusColorMap[currentUserEdit.status].color}
+                    >
+                      {statusColorMap[currentUserEdit.status].text}
+                    </Chip>
+                  </Tooltip>
+                </>
+              )}
+            </div>
+            <div>
+              {currentUserEdit.reasonEntry.length > 0 && (
+                <div>
+                  <p className='text-tiny'>{currentUserEdit.reasonEntry}</p>
+                </div>
+              )}
+              <p className='text-tiny text-foreground-400'>
+                Ingrso: {parsedDate.date} a las {parsedDate.time}
+              </p>
             </div>
           </div>
         </div>
