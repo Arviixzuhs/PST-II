214b6e4c3cc6dd44c59ab511e002de142a2cb408
Arviixzuh <arviixzuh@gmail.com>
2025-01-18 22:59:46 -0400

    feat: ✨ add ConsultsToDayModal to Dashboard for improved consult visibility

--- frontend/src/renderer/src/pages/Dashboard/index.tsx
+++ frontend/src/renderer/src/pages/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import { GraphView } from './GraphView'
 import { DashboardHeader } from './Header'
+import { ConsultsToDayModal } from './Modals/ConsultsToDayModal'
 
 export const Dashboard = () => {
   return (
     <div className='flex flex-col gap-4 h-full'>
       <DashboardHeader />
       <GraphView />
+      <ConsultsToDayModal />
     </div>
   )
 }
