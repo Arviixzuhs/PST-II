1ab8a47dc1122d16fc5fd3967c9c7411014146b2
Arviixzuh <arviixzuh@gmail.com>
2025-01-18 22:59:21 -0400

    feat: ✨ add viewConsultsToDay modal type for improved consult tracking

--- frontend/src/renderer/src/hooks/useModal.ts
+++ frontend/src/renderer/src/hooks/useModal.ts
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { handleShowModal, isModalOpen } from '@renderer/features/currentModal'
 
 export const modalTypes = Object.freeze({
+  viewConsultsToDay: 'VIEW_CONSULTS_TO_DAY',
   editItemTableModal: 'EDIT_ITEM_TABLE_MODAL',
   editItemTableTabModal: 'EDIT_ITEM_TABLE__TAB_MODAL',
   viewPatientProfileModal: 'VIEW_PATIENT_PROFILE_MODAL',
